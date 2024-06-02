d065a2a48ae6ecdee14159bd334cc8e6c456ab9e
Annija Statevica <annija.statevica@gmail.com>
2024-06-02 09:15:24 +0300

    Add: Model Address and Person + Enum City and Size

---
+++ src/main/java/lv/venta/model/Address.java
@@ -0,0 +1,53 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "AddressTable")
+@Entity
+public class Address {
+
+	@Id
+	@Column(name = "Ida")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int ida;
+	
+	@NotNull
+	@Column(name="City")
+	private City city;
+	
+	@NotNull
+	@Min(1)
+	@Column(name = "House_no")
+	private int house_no;
+	
+	@Column(name = "Street_or_house_title")
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String street_or_house_title;
+	
+	public Address(City city, int house_no, String street_or_house_title) {
+		setCity(city);
+		setHouse_no(house_no);
+		setStreet_or_house_title(street_or_house_title);
+	}
+	
+	
+}
---
+++ src/main/java/lv/venta/model/Person.java
@@ -0,0 +1,55 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "PersonTable")
+@Entity
+public class Person {
+
+	@Id
+	@Column(name = "Idp")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int idp;
+	
+	@Column(name = "Name")
+	@NotNull
+	@Size(min = 3, max = 50)
+	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņ]+", message = "Only letters and space are allowed")
+	private String name;
+	
+	@Column(name = "Person_code")
+	@NotNull
+	@Size(min = 12, max = 12)
+	@Pattern(regexp = "[0-9]{6}-[0-9]{5}", message = "Only letters and space are allowed")
+	private String person_code;
+	
+	@Column(name = "Surname")
+	@NotNull
+	@Size(min = 3, max = 50)
+	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņ]+", message = "Only letters and space are allowed")
+	private String surname; 
+	
+	public Person(String name, String person_code,String surname ) {
+		setName(name);
+		setPerson_code(person_code);
+		setSurname(surname);
+	}
+}
