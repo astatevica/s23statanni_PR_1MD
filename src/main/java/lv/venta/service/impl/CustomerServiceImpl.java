b8368dd3a55a2a45854a6f464eb884394136b0e6
Annija Statevica <annija.statevica@gmail.com>
2024-06-03 22:13:59 +0300

    Add: ICustomerService and CustomerServiceImpl

---
+++ src/main/java/lv/venta/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,30 @@
+package lv.venta.service.impl;
+
+
+
+import lv.venta.service.ICustomerService;
+
+public class CustomerServiceImpl implements ICustomerService{
+	
+//	@Autowired
+//	private ICustomer driverRepo;
+
+	@Override
+	public void insertNewCustomerAsPerson(String person_code, String phone_no) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertNewCustomerAsCompany(String company_reg_no, String phone_no, String title) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAddressToCustomerByCustomerId(int id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
